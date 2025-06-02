45a7f74ae95198287c2ab1cd6ab30602cb5ad653
edincom <195357@ecam.be>
2025-06-03 00:51:08 +0200

    Remove unnecessary code

--- a_solid_project/src/components/Sheet.tsx
+++ a_solid_project/src/components/Sheet.tsx
@@ -10,19 +10,6 @@ type SheetProps = {
   showDelete?: boolean;
 };
 
-// export default function Sheet(props: SheetProps) {
-//   return (
-//     <div>
-//       <a
-//         href={`/study/${props.card.id}`}
-//         class="w-[300px] block text-center rounded-full bg-gradient-to-b from-purple-900 to-purple-300 border-2 border-gray-300 focus:border-gray-400 active:border-gray-400 px-[2rem] py-[1rem] text-white"
-//       >
-//         {props.card.title}
-//       </a>
-//     </div>
-//   );
-// }
-
 export default function Sheet(props: SheetProps) {
   return (
     <div class="relative w-[300px] text-center">
